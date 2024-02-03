bba48c65574998b563772522e6a5fe5d682376fb
Maros Tomasov <maros3845@gmail.com>
2024-02-03 19:24:52 +0100

    making changes UNKNOWN

--- ob1/proj1.c
+++ ob1/proj1.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 
-int string_to_int(char *str) {
-    int result = 0;
-
-    for (int i = 0; str[i] != '\0'; i++){
-        result = result * 10 + str[i] - '0';
+char get_input(){
+    char buffer[200];
+    int index = 0;
+    char character;
+        while((character = getchar()) != '\n' && index < (int)sizeof(buffer) - 1){
+        buffer[index++] = character;
     }
-
-    return result;
+    return buffer, index;
 }
+
 
 
 int invalid_argument(){
@@ -18,21 +19,25 @@ int invalid_argument(){
     printf("Pouzi -S a -N [pocet] pre vypis postupnosti v binarnom vstupe \n");
 
     return 0;
-}k
-
-
-void make_something_with_character(char character){
-    printf("%X ", (int)character);
 }
 
+
+
+    
 int main(int argc, char *argv[]){
-    if (argc == 1) {invalid_argument();}
-    printf("argument number: %i \n", string_to_int(argv[2]));
+    char buffer[200];
+    int index = 0;
     char character;
 
-    while((character = getchar()) != '\n'){
-            byte_to_char(character);
+    if (argc == 1) {
+        handle_no_arguments();
     }
+    
+
+
+
+
+    printf("buffer: %s \n", buffer);
 
         switch (argv[1][1]) {
             case 'a':
