778b4b0578d4b33245ca09be96cdc6c2d01b6a55
Maros Tomasov <maros3845@gmail.com>
2024-02-09 13:26:43 +0100

    Work: having the position in HEX and converting to Bytes

--- ob1/proj1.c
+++ ob1/proj1.c
@@ -1,49 +1,76 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int invalid_argument();
+void string_to_bytes();
 
-char get_input(){
-    char buffer[200];
-    int index = 0;
-    char character;
-        while((character = getchar()) != '\n' && index < (int)sizeof(buffer) - 1){
-        buffer[index++] = character;
-    }
-    return buffer;
-}
+void handle_no_arguments();
 
+int main(int argc, char *argv[])
+{
 
-
-int invalid_argument(){
-    char text = get_input();
-    printf("buffer:  %c \n", text);
-    printf("Zadaj validny argument \n");
-    printf("Pouzi -x pre prevod do Hexadecimalnej  \n");
-    printf("Pouzi -S a -N [pocet] pre vypis postupnosti v binarnom vstupe \n");
-
-    return 0;
-}
-
-
-
-    
-int main(int argc, char *argv[]){
-
-    if (argc == 1) {
+    if (argc == 1)
+    {
         handle_no_arguments();
     }
+    else
+    {
+        switch (argv[1][1])
+        {
+        case 'a':
+            printf("you entered -a \n");
+            break;
+        case 'l':
+            printf("you entered -l \n");
+            break;
 
-        switch (argv[1][1]) {
-            case 'a':
-                printf("you entered -a \n");
-                break;
-            case 'l':
-                printf("you entered -l \n");
-                break;
-
-            default:
-                invalid_argument();
-                break;
+        default:
+            handle_no_arguments();
+            break;
         }
+    }
+    printf("\n");
+
     return 0;
+}
+
+void string_to_bytes()
+{
+    char buffer[200];
+    int index;
+    char character;
+    int i;
+    int count = 0;
+
+    while ((character = getchar()) != '\n' && index < (int)sizeof(buffer) - 1){
+        buffer[index++] = character;
+    }
+    printf("Index je: %d \n", index);
+    
+    while (index != 0){
+        printf("\n %08X  ", count);
+        if (index > 16){
+            for (i = 0; i < 16; i++){
+                printf(" %x ", (int)buffer[count]);
+                index--;
+                count++;
+            }
+        }
+        else
+        {
+            for (i = 0; i < index; i++)
+            {
+                printf(" %x ", (int)buffer[count]);
+                count++;
+            }
+            index = 0;
+        }
+        // printf("\n %d \n", i);
+    }
+}
+
+void handle_no_arguments()
+{
+    string_to_bytes();
+    // printf("Zadaj validny argument \n");
+    // printf("Pouzi -x pre prevod do Hexadecimalnej  \n");
+    // printf("Pouzi -S a -N [pocet] pre vypis postupnosti v binarnom vstupe \n");
 }
