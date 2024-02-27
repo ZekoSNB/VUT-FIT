8ba5bcf91e2c97854296bd1bb6d5873e0f5f1c66
Maros Tomasov <maros3845@gmail.com>
2024-02-27 20:52:58 +0100

    testing hex to dec

--- ob1/testing.c
+++ ob1/testing.c
@@ -1,12 +1,44 @@
-#include <stdio.h>
-
-int main() {
-    unsigned char hex_bytes[] = {0x20, 0x6a, 0x6c}; // Bytes represented in hexadecimal format
-    char ascii_string[3]; // Assuming 2 bytes -> 2 characters + '\0'
-
-    sprintf(ascii_string, "%c%c", hex_bytes[1], hex_bytes[2]); // Convert bytes to ASCII characters
-
-    printf("ASCII string: %s\n", ascii_string);
- 
-    return 0;
+// C program to demonstrate hexadecimal to decimal 
+// conversion using for loop 
+#include <math.h> 
+#include <stdio.h> 
+// #include <string.h> 
+int main() { 
+  
+    char hexdecnumber[17] = "2D"; 
+    long  decimalnumber;
+    int i = 0, val, len; 
+  
+    decimalnumber = 0; 
+  
+    // Find the length of total number of hex digit 
+    // finding the length of hexa decimal number 
+    len = sizeof(hexdecnumber); 
+    len--; 
+  
+    // for loop iterates the hexa decimal number digits 
+    for (i = 0; hexdecnumber[i] != '\0'; i++) { 
+  
+        // finding the equivalent decimal digit for each 
+        // hexa decimal digit 
+        if (hexdecnumber[i] >= '0'
+            && hexdecnumber[i] <= '9') { 
+            val = hexdecnumber[i] - 48; 
+        } 
+        else if (hexdecnumber[i] >= 'a'
+                 && hexdecnumber[i] <= 'f') { 
+            val = hexdecnumber[i] - 97 + 10; 
+        } 
+        else if (hexdecnumber[i] >= 'A'
+                 && hexdecnumber[i] <= 'F') { 
+            val = hexdecnumber[i] - 65 + 10; 
+        } 
+  
+        decimalnumber += val * pow(16, len); 
+        len--; 
+    } 
+    // printing the result 
+    printf("Hexadecimal number = %s\n", hexdecnumber); 
+    printf("Decimal number = %ld", decimalnumber); 
+    return 0; 
 }
